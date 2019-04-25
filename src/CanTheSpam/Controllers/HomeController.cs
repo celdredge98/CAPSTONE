ebb711b3125422b273eac8ceb94f035674f04245
Cody <celdredge98@gmail.com>
2019-04-24 21:53:03 -0600

    Reworked email form. Created ValidateEmail page. Created ValidateEmail controller to post userEmail to validateEmail page.

--- src/CanTheSpam/Controllers/HomeController.cs
+++ src/CanTheSpam/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace CanTheSpam.Controllers
          _logger = logger;
       }
 
-      public async Task<IActionResult> Index()
+      public IActionResult Index()
       {
          _logger.LogDebug($"{GetType().Name}.{nameof(Index)} method called...");
 
@@ -25,6 +25,37 @@ namespace CanTheSpam.Controllers
       public IActionResult Privacy()
       {
          _logger.LogDebug($"{GetType().Name}.{nameof(Privacy)} method called...");
+
+         return View();
+      }
+
+      [HttpPost]
+      public IActionResult ValidateEmail(UserEmailDetails userEmail)
+      {
+         _logger.LogDebug($"{GetType().Name}.{nameof(Privacy)} method called...");
+
+         ViewData["Email"] = userEmail.Email;
+
+         // Check for robots using captcha
+         //    IF Not Robot continue on
+         //    ELSE Robot Goto I hate robot's page --> Page redirect -- Log IP Address, Log in Robot submitted Email table
+         // Save email in database
+         // mark email with tentative approval pending validation
+         // Send email to user using provided email address with "Magic" link to prove ownership
+         // User can use the link from email to validate and then it redirects to "Thank you page"
+         // or user can enter the code on this page (Validate) page and then goto "Thank you page"
+
+         // https://CansTheSpam.com/ValidateEmail?e=user@example.com&v=37AB7F39-CC4D-40DC-B113-DA9452501B1D ([HttpGet])
+         // https://CansTheSpam.com/ValidateEmail/?e=user@example.com&v=37AB7F39-CC4D-40DC-B113-DA9452501B1D ([HttpGet])
+
+         return View();
+      }
+
+      [HttpGet]
+      public IActionResult ValidateEmail([FromQuery] string e, [FromQuery] string v)
+      {
+         // IF email link used and is valid automatically redirect to "Thank You" page
+         // ELSE stay on page and report error to use that it failed to validate and try again.
 
          return View();
       }
---
+++ src/CanTheSpam/Models/UserEmailDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CanTheSpam.Models
+{
+    public class UserEmailDetails
+    {
+        [Required]
+        [Display(Name = "Enter Personal Email")]
+        [RegularExpression(@"^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$", ErrorMessage = "Please enter a valid Email address.")]
+        [MaxLength(256, ErrorMessage = "Email is to long.")]
+        [MinLength(3, ErrorMessage = "Email does not meat minimum length.")]
+        public string Email { get; set; }
+    }
+}
